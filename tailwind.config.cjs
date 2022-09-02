78b6b08354efccdd52b5878a1c7884acc9be730d
Chickenbiscuitoo <chickenbiscuito.dev@gmail.com>
2022-09-02 12:52:47 +0200

    added daisyui and custom theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
+	content: ['./src/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {},
+	},
+	plugins: [require('daisyui')],
+	daisyui: {
+		themes: [
+			{
+				mytheme: {
+					primary: '#565676',
+
+					secondary: '#9CA3DB',
+
+					accent: '#4e4e94',
+
+					neutral: '#9CC0E7',
+
+					'base-100': '#A8AEC1',
+
+					info: '#3ABFF8',
+
+					success: '#36D399',
+
+					warning: '#FBBD23',
+
+					error: '#F87272',
+				},
+			},
+		],
+	},
+}
